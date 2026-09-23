1c9b637cc136bb78c078940389720d41beded731
Christos G <xgeo2000@pm.me>
2026-09-22 18:28:30 -0700

    Finished basic layout for app
    Now just to grab some assets and make up some affirmations
    
    Co-Authored-By: Britney F. <197664430+PhantomWolf13@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,74 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //runApp(const MyApp());
+  runApp(const AffirmationsApp());
 }
 
+// Creating an "Affirmation" class to represent the contents of our cards
+class Affirmation {
+  final String text;
+  final String imageAsset;
+
+  const Affirmation({required this.text, required this.imageAsset});
+}
+
+// Our main app, this is what main executes runApp on :)
+class AffirmationsApp extends StatelessWidget {
+  const AffirmationsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // No title this time because I'm pretty sure that the AppBar overwrites it
+      home: AffirmationsScreen(),
+    );
+  }
+}
+
+// Main screen of our app, stateless widget, gets fed into the build
+class AffirmationsScreen extends StatelessWidget {
+  const AffirmationsScreen({super.key});
+
+  final List<Affirmation> affirmations = const [
+    Affirmation(text: "My schemes are masterful", imageAsset: "assets/"),
+    // TODO: Add more
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Britney and Christos' Affirmations"),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+      ),
+      // Here's the most important part, our ListView.builder, it constructs
+      // Our scrollable list
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: affirmations.length,
+        itemBuilder: (context, index) {
+          final affirmation = affirmations[index];
+          // Populating the ListView with a List<affirmations>
+          return Card(
+            margin: const EdgeInsets.only(bottom: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset(
+                  affirmation.imageAsset,
+                  height: 220,
+                  //fit:
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: Text(affirmation.text, style: TextStyle(fontSize: 24)),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
